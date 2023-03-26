bb5027144d123780f0d632fe6c21342eda0228a7
chengyen.chiang <chengyen.chiang@leediancn.com>
2023-03-27 00:01:01 +0800

    notification

--- app/src/main/java/com/example/realtor_lease_platform_android/tool/StringProcess.java
+++ app/src/main/java/com/example/realtor_lease_platform_android/tool/StringProcess.java
@@ -3,8 +3,9 @@ package com.example.realtor_lease_platform_android.tool;
 import com.example.realtor_lease_platform_android.define.Constants;
 import com.example.realtor_lease_platform_android.role.Config;
 
-public class StringProcess {
+import org.json.JSONObject;
 
+public class StringProcess {
     public static String getQueryConfigWhereStringByPK(Config config) {
         return Constants.ID_SQL + "='" + config.getId() + "'";
     }
@@ -18,6 +19,10 @@ public class StringProcess {
     public static String getJavascriptFunctionSetAccountPasswordUrlString(String account,String password) {
         String arg = account + "','" + password;
         return getJavascriptFunctionString(arg,Constants.SET_ACCOUNT_PASSWORD_JAVASCRIPT);
+    }
+
+    public static String getReserveHouseUrl(String reserveHouseId) {
+        return Constants.SERVER_URL + "21/" + reserveHouseId;
     }
 
 }
